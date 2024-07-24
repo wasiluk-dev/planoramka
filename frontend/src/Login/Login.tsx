d4e4fc23ea2622412803994e4ce78321a4cf2316
CinekTV <cinekv15@gmail.com>
2024-07-24 16:18:32 +0200

    Formularz

--- frontend/src/Login/Login.tsx
+++ frontend/src/Login/Login.tsx
@@ -1,30 +1,47 @@
 import React from "react";
 
 const Login: React.FC = () =>{
-    let name: string | null = null;
+    let ulogin: string | null = null;
     let pass: string | null = null;
 
     const login = () => {
-        const nameElement = document.getElementById('name') as HTMLInputElement | null;
+        const uloginElement = document.getElementById('login') as HTMLInputElement | null;
         const passElement = document.getElementById('pass') as HTMLInputElement | null;
-        if (nameElement && nameElement.value && passElement && passElement.value) {
-            name= nameElement.value;
+        //jeśli istnieje to weź wartość name i pass
+        if (uloginElement && uloginElement.value && passElement && passElement.value) {
+            ulogin= uloginElement.value;
             pass= passElement.value;
         }
-        console.log(name);
+        console.log("Logowanie:");
+        console.log(ulogin);
         console.log(pass);
     }
 
     const register = () =>{
-        console.log("Zarejestrowałeś się mordo");
+        const uloginElement = document.getElementById('login') as HTMLInputElement | null;
+        const passElement = document.getElementById('pass') as HTMLInputElement | null;
+        //jeśli istnieje to weź wartość name i pass
+        if (uloginElement && uloginElement.value && passElement && passElement.value) {
+            ulogin= uloginElement.value;
+            pass= passElement.value;
+        }
+        console.log("Rejestracja:");
+        console.log(ulogin);
+        console.log(pass);
     }
  return(
      <>
          <form>
-             <label htmlFor="name">Login:</label><br/>
+             <label htmlFor="login">Login:</label><br/>
+             <input type="text" id="login" name="login"/><br/>
+             <label htmlFor="pass">Hasło:</label><br/>
+             <input type="password" id="pass" name="pass"/><br/>
+             <label htmlFor="name">Imię:</label><br/>
              <input type="text" id="name" name="name"/><br/>
-             <label htmlFor="lname">Hasło:</label><br/>
-             <input type="password" id="pass" name="pass"/>
+             <label htmlFor="sname">Nazwisko:</label><br/>
+             <input type="surname" id="sname" name="sname"/><br/>
+             <label htmlFor="email">Email:</label><br/>
+             <input type="email" id="email" name="email"/>
          </form>
          <button name='login' onClick={login}>Zaloguj się</button>
          <button name='register' onClick={register}>Zarejestruj się</button>
