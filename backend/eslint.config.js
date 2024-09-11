20b11923e6f83cd02b0d9422db55094fb388aee4
Gabriel Wasiluk <17027711+gabwasik@users.noreply.github.com>
2024-09-11 11:05:29 +0200

    Updated package.json and eslint.config.js

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,5 +1,6 @@
 import { fixupConfigRules } from '@eslint/compat';
 import eslintJs from '@eslint/js';
+import stylisticJs from '@stylistic/eslint-plugin-js'
 import eslintImport from 'eslint-plugin-import';
 import docJs from 'eslint-plugin-jsdoc';
 import eslintReact from 'eslint-plugin-react/configs/recommended.js';
@@ -22,9 +23,30 @@ export default [
             globals: globals.node
         },
         plugins: {
-            import: eslintImport
+            '@stylistic/js': stylisticJs,
+            import: eslintImport,
         },
         rules: {
+            // === Stylistic ===
+            // spacing
+            '@stylistic/js/keyword-spacing': ['error', { 'after': true, 'before': true }],
+
+            // line breaks
+            '@stylistic/js/eol-last': ['error', 'always'],
+
+            // brackets
+            '@stylistic/js/brace-style': 'error',
+
+            // quotes
+            '@stylistic/js/quotes': ['error', 'single'],
+
+            // disallow
+            '@stylistic/js/no-floating-decimal': 'error',
+            '@stylistic/js/no-mixed-operators': 'error',
+            '@stylistic/js/no-trailing-spaces': 'error',
+            '@stylistic/js/no-whitespace-before-property': 'error',
+
+            // === Imports ===
             'import/order': ['error', {
                 'groups': [['builtin', 'external'], 'internal', 'parent', 'sibling', 'index'],
                 'alphabetize': { order: 'asc', caseInsensitive: true },
