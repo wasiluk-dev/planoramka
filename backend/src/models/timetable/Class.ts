ebcccff26d2a1b72a373e95801b87e7d9e975a17
Gabriel Wasiluk <17027711+gabwasik@users.noreply.github.com>
2024-09-11 20:29:34 +0200

    Fitted the models to example data

--- backend/src/models/timetable/Class.ts
+++ backend/src/models/timetable/Class.ts
@@ -1,24 +1,41 @@
 import { HydratedDocumentFromSchema, Schema } from 'mongoose';
 
 import Base from '../Base';
-import { SubjectSchema } from '../courses/Subject';
-import { RoomSchema } from '../faculty/Room';
-import { ClassTypeSchema } from './ClassType';
-import { PeriodSchema } from './Period';
+import Subject from '../courses/Subject';
+import Room from '../faculty/Room';
+import User from '../User';
+import ClassType from './ClassType';
+import Period from './Period';
 
 export const ClassSchema = new Schema({
+    organizer: {
+        type: Schema.Types.ObjectId,
+        ref: new User().name,
+        required: true,
+    },
     subject: {
-        type: SubjectSchema,
+        type: Schema.Types.ObjectId,
+        ref: new Subject().name,
     },
     classType: {
-        type: ClassTypeSchema,
+        type: Schema.Types.ObjectId,
+        ref: new ClassType().name,
     },
+    studentGroups: [{
+        type: Number,
+        min: 1,
+        validate: {
+            validator: Number.isInteger,
+        },
+    }],
     room: {
-        type: RoomSchema,
+        type: Schema.Types.ObjectId,
+        ref: new Room().name,
         required: true,
     },
     periods: [{
-        type: PeriodSchema,
+        type: Schema.Types.ObjectId,
+        ref: new Period().name,
         required: true,
     }],
 });
--- backend/src/models/timetable/Timetable.ts
+++ backend/src/models/timetable/Timetable.ts
@@ -1,9 +1,21 @@
 import { HydratedDocumentFromSchema, Schema } from 'mongoose';
 
 import Base from '../Base';
+import Semester from '../courses/Semester';
 import Class from './Class';
 
 export const TimetableSchema = new Schema({
+    semester: {
+        type: Schema.Types.ObjectId,
+        ref: new Semester().name,
+    },
+    targetedSemester: {
+        type: Number,
+        min: 1,
+        validate: {
+            validator: Number.isInteger,
+        },
+    },
     classes: [{
         type: Schema.Types.ObjectId,
         ref: new Class().name,
