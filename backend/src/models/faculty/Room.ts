6b12811eac96125a9adebc86f44578d9ec4bc1a1
Gabriel Wasiluk <17027711+gabwasik@users.noreply.github.com>
2024-09-12 16:26:57 +0200

    Added virtuals to Room and User

--- backend/src/models/faculty/Room.ts
+++ backend/src/models/faculty/Room.ts
@@ -20,6 +20,21 @@ export const RoomSchema = new Schema({
     },
 });
 
+RoomSchema.virtual('roomNumber')
+    .get(function() {
+        if (!this.number && !this.numberSecondary) {
+            return null;
+        }
+
+        if (!this.numberSecondary) {
+            return this.number;
+        } else if (!this.number) {
+            return this.numberSecondary;
+        } else {
+            return `${this.number} (${this.numberSecondary})`;
+        }
+    });
+
 class Room extends Base<HydratedDocumentFromSchema<typeof RoomSchema>> {
     constructor() {
         super('Room', RoomSchema);
